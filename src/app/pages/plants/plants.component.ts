743c5e07e7070d81bc212cc330fd82f6c260b414
Natalia Paredes Muñoz <nparedes@alltic.com.co>
2025-06-15 20:31:55 -0500

    Se agrega carrusel en plants y maqueta para el catalogo

--- src/app/pages/plants/plants.component.ts
+++ src/app/pages/plants/plants.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
 @Component({
   selector: 'app-plants',
@@ -8,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class PlantsComponent {
   public email: string = 'Feel free to reach out at aep.architecture@gmail.com —'
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+    this.initSplide();
+  }, 50);
+  }
+
+  
+  initSplide() {
+    const splide = new Splide( '.splide', {
+        type   : 'loop',
+        drag   : 'free',
+        focus  : 'center',
+        perPage: 4,
+        autoScroll: {
+          speed: -1,
+        },
+      } );
+      splide.mount({ AutoScroll });
+    }
 }
