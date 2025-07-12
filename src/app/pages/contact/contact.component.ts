f1b8563cde4ff57c5287a96d2c0c02add5a8d273
Natalia Paredes Muñoz <nparedes@alltic.com.co>
2025-07-11 21:31:20 -0500

    Se agrega funcionalidad para el formulario de contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
   public email: string = "aep.architecture@gmail.com"
+
+  sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_sekzynt',
+      'template_3px7sfd',
+      e.target as HTMLFormElement,
+      { publicKey: 'hr-2Arl25Z7ZbuI6q' }
+    )
+    .then((res: EmailJSResponseStatus) => {
+      alert('The email has been successfully sent');
+      (e.target as HTMLFormElement).reset();
+    })
+    .catch(err => {
+      alert('Failed to send the email');
+    });
+  }
 }
